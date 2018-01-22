2262a55c8ec533b70c37a50acfb673e02b32e393
suna <macsss@gmail.com>
2018-01-23 01:18:11 +0900

    Change the HTML for the site.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+#post_list라는 함수(def) 만들어 요청(request)을 넘겨받아 render메서드를 호출합니다.
+#이 함수는 호출하여 받은(return) blog/post_list.html템플릿을 보여줍니다.
